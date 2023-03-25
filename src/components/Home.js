af24c5e4e3ff2fbf4629e53ea2e192efcc502044
ratul-islam <hasanrafiul32@gmail.com>
2023-03-25 23:59:37 +0600

    removed all unnesessary api cals

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
-import React,{useState, useEffect} from 'react';
+import React from 'react';
 
-import {v4 as uuidv4} from 'uuid';
 import Countrys from "./Countrys"
 
 
@@ -13,7 +12,7 @@ const Home = (props) => {
         {isLoading && <h2>Loading........</h2>}
         {error && <h2>{error}</h2>}
         {datas.map(data => <Countrys remove={remove} id={data.id} key={data.id}  flag={data.data.flags} name={data.data.name} capital={data.data.capital
-        } region={data.data.region} currencies={data.data.currencies} languages={data.data.languages} population={data.data.population}/>)}
+        } region={data.data.region} population={data.data.population}/>)}
 
     </main>
   )
